def045e6258545b9a89a6933a39ca02edd8a27fd
Jacob Stenum Czepluch <j.czepluch@proton.me>
2025-10-08 14:16:19 +0200

    fix tests, remove modifier from protocol

--- assertions/src/KYCAssertion.a.sol
+++ assertions/src/KYCAssertion.a.sol
@@ -8,37 +8,61 @@ import {AccreditedInvestorRegistry} from "../../src/AccreditedInvestorRegistry.s
 
 /// @notice Assertions ensuring OnlyAccreditedCanMint restricts privileged actions to accredited callers
 contract OnlyAccreditedCanMintAssertion is Assertion {
-    OnlyAccreditedCanMint public immutable token;
     AccreditedInvestorRegistry public immutable registry;
 
     constructor(address tokenAddress) {
-        token = OnlyAccreditedCanMint(tokenAddress);
-        registry = token.registry();
+        registry = AccreditedInvestorRegistry(tokenAddress);
     }
 
     // register the functions that will be called and used by the assertion functions below
     function triggers() external view override {
-        registerCallTrigger(this.assertOnlyAccreditedMint.selector, OnlyAccreditedCanMint.mint.selector);
-        registerCallTrigger(this.assertOnlyAccreditedTransfer.selector, OnlyAccreditedCanMint.transfer.selector);
+        registerCallTrigger(
+            this.assertOnlyAccreditedMint.selector,
+            OnlyAccreditedCanMint.mint.selector
+        );
+        registerCallTrigger(
+            this.assertOnlyAccreditedTransfer.selector,
+            OnlyAccreditedCanMint.transfer.selector
+        );
     }
 
     /// @notice When msg.sender hits mint, if they are not in the registry, theycannot mint tokens
     function assertOnlyAccreditedMint() external {
-        PhEvm.CallInputs[] memory calls = ph.getCallInputs(address(token), OnlyAccreditedCanMint.mint.selector);
+        OnlyAccreditedCanMint token = OnlyAccreditedCanMint(
+            ph.getAssertionAdopter()
+        );
+
+        PhEvm.CallInputs[] memory calls = ph.getCallInputs(
+            address(token),
+            OnlyAccreditedCanMint.mint.selector
+        );
 
         for (uint256 i = 0; i < calls.length; i++) {
             // Bundle the accreditation check into any call
-            require(registry.isAccredited(calls[i].caller), "caller is not accredited");
+            require(
+                registry.isAccredited(calls[i].caller),
+                "caller is not accredited"
+            );
         }
     }
 
     /// @notice Ensure that non-accredited callers cannot transfer tokens
     function assertOnlyAccreditedTransfer() external {
-        PhEvm.CallInputs[] memory calls = ph.getCallInputs(address(token), OnlyAccreditedCanMint.transfer.selector);
+        OnlyAccreditedCanMint token = OnlyAccreditedCanMint(
+            ph.getAssertionAdopter()
+        );
+
+        PhEvm.CallInputs[] memory calls = ph.getCallInputs(
+            address(token),
+            OnlyAccreditedCanMint.transfer.selector
+        );
 
         for (uint256 i = 0; i < calls.length; i++) {
             // Bundle the accreditation check into any call
-            require(registry.isAccredited(calls[i].caller), "caller is not accredited");
+            require(
+                registry.isAccredited(calls[i].caller),
+                "caller is not accredited"
+            );
         }
     }
 }
--- assertions/test/KYCAssertion.t.sol
+++ assertions/test/KYCAssertion.t.sol
@@ -1,19 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import {CredibleTest} from "credible-std/CredibleTest.sol";
+import {Test} from "forge-std/Test.sol";
 import {OnlyAccreditedCanMintAssertion as KYCAssertion} from "../src/KYCAssertion.a.sol";
-import {PhylaxTestBase, MockOnlyAccreditedCanMint} from "./PhylaxTestBase.t.sol";
+import {OnlyAccreditedCanMint} from "../../src/OnlyAccreditedCanMint.sol";
+import {AccreditedInvestorRegistry} from "../../src/AccreditedInvestorRegistry.sol";
 
-contract KYCAssertionTest is PhylaxTestBase {
-    address private constant RECIPIENT = address(0xCAFE);
+contract KYCAssertionTest is Test, CredibleTest {
+    AccreditedInvestorRegistry internal registry;
+    OnlyAccreditedCanMint internal token;
+
+    address internal constant ADMIN = address(0xA11CE);
+    address internal constant RECIPIENT = address(0xCAFE);
+    address internal constant ACCREDITED = address(0xACCE);
+    address internal constant NON_ACCREDITED = address(0xBEEF);
+
+    function setUp() public virtual {
+        registry = new AccreditedInvestorRegistry(ADMIN);
+        token = new OnlyAccreditedCanMint("Phylax", "PHYX", registry);
+        vm.deal(ADMIN, 100 ether);
+        vm.deal(ACCREDITED, 100 ether);
+        vm.deal(NON_ACCREDITED, 100 ether);
+    }
 
     // allow cl.assertion to access variables in the constructor
     function _createData() private view returns (bytes memory) {
-        return abi.encodePacked(type(KYCAssertion).creationCode, abi.encode(address(token)));
+        return
+            abi.encodePacked(
+                type(KYCAssertion).creationCode,
+                abi.encode(address(registry))
+            );
     }
 
     function testMintAllowsAccreditedCaller() public {
-        accredit(ACCREDITED);
+        vm.prank(ADMIN);
+        registry.addInvestor(ACCREDITED);
 
         cl.assertion({
             adopter: address(token),
@@ -38,7 +60,8 @@ contract KYCAssertionTest is PhylaxTestBase {
     }
 
     function testTransferAllowsAccreditedCaller() public {
-        accredit(ACCREDITED);
+        vm.prank(ADMIN);
+        registry.addInvestor(ACCREDITED);
         vm.prank(ADMIN);
         token.mint(ACCREDITED, 5 ether);
 
@@ -64,6 +87,62 @@ contract KYCAssertionTest is PhylaxTestBase {
 
         vm.expectRevert("caller is not accredited");
         vm.prank(NON_ACCREDITED);
+        token.transfer(RECIPIENT, 1 ether);
+    }
+
+    function testMintRevertsAfterRevocation() public {
+        vm.prank(ADMIN);
+        registry.addInvestor(ACCREDITED);
+
+        cl.assertion({
+            adopter: address(token),
+            createData: _createData(),
+            fnSelector: KYCAssertion.assertOnlyAccreditedMint.selector
+        });
+
+        vm.prank(ACCREDITED);
+        token.mint(RECIPIENT, 1 ether);
+
+        vm.prank(ADMIN);
+        registry.removeInvestor(ACCREDITED);
+
+        cl.assertion({
+            adopter: address(token),
+            createData: _createData(),
+            fnSelector: KYCAssertion.assertOnlyAccreditedMint.selector
+        });
+
+        vm.expectRevert("caller is not accredited");
+        vm.prank(ACCREDITED);
+        token.mint(RECIPIENT, 1 ether);
+    }
+
+    function testTransferRevertsAfterRevocation() public {
+        vm.prank(ADMIN);
+        registry.addInvestor(ACCREDITED);
+        vm.prank(ADMIN);
+        token.mint(ACCREDITED, 5 ether);
+
+        cl.assertion({
+            adopter: address(token),
+            createData: _createData(),
+            fnSelector: KYCAssertion.assertOnlyAccreditedTransfer.selector
+        });
+
+        vm.prank(ACCREDITED);
+        token.transfer(RECIPIENT, 2 ether);
+
+        vm.prank(ADMIN);
+        registry.removeInvestor(ACCREDITED);
+
+        cl.assertion({
+            adopter: address(token),
+            createData: _createData(),
+            fnSelector: KYCAssertion.assertOnlyAccreditedTransfer.selector
+        });
+
+        vm.expectRevert("caller is not accredited");
+        vm.prank(ACCREDITED);
         token.transfer(RECIPIENT, 1 ether);
     }
 }
